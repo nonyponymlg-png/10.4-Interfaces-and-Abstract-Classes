ffd99812d9d0e52155bab9e1171f62ceb407de2a
nonyponymlg-png <nonyponymlg@gmail.com>
2026-02-05 10:18:05 -0600

    Convert Entity class to Entity interface
    Refactored Entity class into an interface, removing the location field and constructor.

--- Entity.java
+++ Entity.java
@@ -1,13 +1,4 @@
-public class Entity {
-  String location;
-  
-  constructor(String _location) {
-    location = _location;
-  }
-
-  void setLocation(x) {location=x}
-  String getLocation() {return location}
-  
-  abstract void interact(); // Like pressing "e"
-  abstract void update();
+public interface Entity {
+  void interact(); // Like pressing "e"
+  void update();
 }
