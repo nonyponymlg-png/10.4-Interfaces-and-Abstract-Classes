48dd0aceda623089ea9c07bffef0ffb0560ffa0c
nonyponymlg-png <nonyponymlg@gmail.com>
2026-02-05 22:14:47 -0600

    Implement Main class with animal interactions

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+      Dog myDog = new Dog("Clam", 1498134);
+      Cat myCat = new Cat("Frank", 323570);
+      Person myFriend = new Cat("Aiden", 323570);
+      Person myBFF = new Cat("Eli", 0);
+      myDog.interact();
+      myDog.sound();
+      myCat.interact();
+      myCat.sound();
+      myFriend.interact();
+      myBFF.interact();
+      
+    }
+}
