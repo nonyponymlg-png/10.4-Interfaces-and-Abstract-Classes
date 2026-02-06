dca414907a46a40d638fb798cfc3f43be6334ec9
nonyponymlg-png <nonyponymlg@gmail.com>
2026-02-05 22:23:15 -0600

    Fix instantiation of Person objects in Main

--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ public class Main {
     public static void main(String[] args) {
       Dog myDog = new Dog("Clam", 1498134);
       Cat myCat = new Cat("Frank", 323570);
-      Person myFriend = new Cat("Aiden", 323570);
-      Person myBFF = new Cat("Eli", 0);
+      Person myFriend = new Person("Aiden", 323570);
+      Person myBFF = new Person("Eli", 0);
       myDog.interact();
       myDog.sound();
+      System.out.println();
       myCat.interact();
       myCat.sound();
+      System.out.println();
       myFriend.interact();
+      System.out.println();
       myBFF.interact();
       
     }
